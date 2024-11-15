e6fa578085f9f292794b5a1ec673653d9159a04f
Iryna Ilina <irynailina@yahoo.com>
2024-11-14 18:06:18 -0800

    final consts

--- src/main/java/net/restcall/consts/RcConsts.java
+++ src/main/java/net/restcall/consts/RcConsts.java
@@ -1,10 +1,13 @@
 package net.restcall.consts;
 
-public class RcConsts {
+public final class RcConsts {
 
 	public static final String version = "1.0.1";
 
 	public enum RequestTypes {
 		GET, POST, PUT, DELETE;
+	}
+
+	private RcConsts() {
 	}
 }
