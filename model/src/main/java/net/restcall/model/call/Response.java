6e2a35835408e403a5d07a3046896a2c77f9b3c4
Iryna Ilina <irynailina@yahoo.com>
2024-12-09 19:02:36 -0800

    STORY[#187254486] basic engine

--- model/src/main/java/net/restcall/model/call/Response.java
+++ model/src/main/java/net/restcall/model/call/Response.java
@@ -7,7 +7,51 @@ import net.restcall.model.call.response.TestResults;
 
 public class Response {
 	private Body body;
+
 	private Headers headers;
 	private Cookies cookies;
 	private TestResults testResults;
+
+	public Response() {
+	}
+
+	public Response(Body body, Headers headers, Cookies cookies, TestResults testResults) {
+		super();
+		this.body = body;
+		this.headers = headers;
+		this.cookies = cookies;
+		this.testResults = testResults;
+	}
+
+	public Body getBody() {
+		return body;
+	}
+
+	public void setBody(Body body) {
+		this.body = body;
+	}
+
+	public Headers getHeaders() {
+		return headers;
+	}
+
+	public void setHeaders(Headers headers) {
+		this.headers = headers;
+	}
+
+	public Cookies getCookies() {
+		return cookies;
+	}
+
+	public void setCookies(Cookies cookies) {
+		this.cookies = cookies;
+	}
+
+	public TestResults getTestResults() {
+		return testResults;
+	}
+
+	public void setTestResults(TestResults testResults) {
+		this.testResults = testResults;
+	}
 }
--- model/src/main/java/net/restcall/model/call/response/Body.java
+++ model/src/main/java/net/restcall/model/call/response/Body.java
@@ -1,5 +1,19 @@
 package net.restcall.model.call.response;
 
 public class Body {
-	private String response;
+	private String payload;
+	
+
+	public Body(String payload) {
+		super();
+		this.payload = payload;
+	}
+
+	public String getPayload() {
+		return payload;
+	}
+
+	public void setPayload(String payload) {
+		this.payload = payload;
+	}
 }
