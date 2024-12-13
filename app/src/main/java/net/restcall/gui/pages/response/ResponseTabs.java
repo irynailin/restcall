e99446c7c5348b8a1ddbc84ff6baa971a3e8729f
Iryna Ilina <irynailina@yahoo.com>
2024-12-12 20:13:37 -0800

    STORY[#188685233] response body ui

--- app/src/main/java/net/restcall/gui/pages/response/ResponseTabs.java
+++ app/src/main/java/net/restcall/gui/pages/response/ResponseTabs.java
@@ -3,10 +3,12 @@ package net.restcall.gui.pages.response;
 import javax.swing.JLabel;
 import javax.swing.JTabbedPane;
 
+import net.restcall.gui.pages.response.tabs.ResponseBody;
+
 public class ResponseTabs extends JTabbedPane {
 	public ResponseTabs() {
 		super(BOTTOM, SCROLL_TAB_LAYOUT);
-		addTab("Body", new JLabel("dhsoif"));
+		addTab("Body", new ResponseBody());
 		addTab("Cookies", new JLabel("dhsoif"));
 		addTab("Headers", new JLabel("dhsoif"));
 		addTab("Test Results", new JLabel("dhsoif"));
